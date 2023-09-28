4e2771749cd356e33990a60b2d94adf4fb1ec3c5
JT Reyes <jesustgreyes@gmail.com>
2023-09-27 21:59:13 -0700

    create connection to mysql database

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const mysql = require('mysql2');
 const inquirer = require('inquirer');
 const { promisify } = require('util');
 
+const db = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    password: 'password',
+    database: 'employees_db',
+});
+
 async function promptUser(){
     let answers = await inquirer.prompt([
         {
