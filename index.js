e650e0f529b4645c99b9f8b3894f4940aa491fa1
JT Reyes <jesustgreyes@gmail.com>
2023-09-27 21:54:15 -0700

    add prompt user function

---
+++ index.js
@@ -0,0 +1,12 @@
+async function promptUser(){
+    let answers = await inquirer.prompt([
+        {
+            type: 'list',
+            name: 'action',
+            message: "What would you like to choose?",
+            choices: ["view all departments", "view all roles", 
+            "view all employees", "add a department", "add a role", "add an employee", "update an employee role","quit"]
+        }
+    ]);
+    return answers;
+}
