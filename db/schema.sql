9b6997d54feb8f2fdaf2252f54e72c62104e0967
JT Reyes <jesustgreyes@gmail.com>
2023-09-27 21:50:34 -0700

    add employee table

--- db/schema.sql
+++ db/schema.sql
@@ -7,3 +7,21 @@ CREATE TABLE department(
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL
 );
+
+CREATE TABLE role(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
+);
+
+CREATE TABLE employee(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NUll,
+    role_id INT,
+    FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
+    manager_id INT,
+    FOREIGN KEY (manager_id) REFERENCES employee (id)
+);
