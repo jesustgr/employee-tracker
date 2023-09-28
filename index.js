1b6feeb062e81f942ad3c44a372839f0dd5345ef
JT Reyes <jesustgreyes@gmail.com>
2023-09-27 21:58:23 -0700

    add dependencies to index.js

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
+const { promisify } = require('util');
+
 async function promptUser(){
     let answers = await inquirer.prompt([
         {
             type: 'list',
             name: 'action',
-            message: "What would you like to choose?",
+            message: "Please choose an action.",
             choices: ["view all departments", "view all roles", 
             "view all employees", "add a department", "add a role", "add an employee", "update an employee role","quit"]
         }
